3b12d60ca952f9e4f93048d59bd397a8bf5b7470
Everton Fernandes <evertonfernandes.dev@gmail.com>
2024-02-12 15:40:22 -0300

    feat: :sparkles: add new methods to the repository

--- src/test/repositories/in-memory-questions-repository.ts
+++ src/test/repositories/in-memory-questions-repository.ts
@@ -1,35 +1,49 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
-    public items: Question[] = []
+  public items: Question[] = []
 
-    async findById(id: string) {
-        const question = this.items.find((item) => item.id.toString() === id)
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
 
-        if (!question) {
-            return null
-        }
-
-        return question
+    if (!question) {
+      return null
     }
 
-    async findBySlug(slug: string) {
-        const question = this.items.find((item) => item.slug.value === slug)
+    return question
+  }
 
-        if (!question) {
-            return null
-        }
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
 
-        return question
+    if (!question) {
+      return null
     }
 
-    async create(question: Question) {
-        this.items.push(question)
-    }
+    return question
+  }
 
-    async delete(question: Question) {
-        const itemIndex = this.items.findIndex(item => item.id === question.id)
-        this.items.splice(itemIndex, 1)
-    }
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+
+  async create(question: Question) {
+    this.items.push(question)
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items[itemIndex] = question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items.splice(itemIndex, 1)
+  }
 }
